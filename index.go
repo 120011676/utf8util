4d3be45e59c71d339164f2f644ed4b88262a371c
海东总司 <1252658389@qq.com>
2019-10-09 18:18:47 +0800

    Update index.go

--- index.go
+++ index.go
@@ -1,21 +1,28 @@
 package hdUnicode
 
-func Index(str, substr string) int {
-	pos := 0
-	i := 0
-	substr_len := len(substr)
-	str_len := len(str)
-	for i < str_len {
-		if str[i] == substr[0] {
-			if str[i:i+substr_len] == substr {
-				return pos
+import "strings"
+
+func UniIndex(str, substr string) int {
+	switch strings.Index(str, substr) {
+	case 0:
+		return 0
+	default:
+		pos := 0
+		i := 0
+		substr_len := len(substr)
+		str_len := len(str)
+		for i < str_len {
+			if str[i] == substr[0] {
+				if str[i:i+substr_len] == substr {
+					return pos
+				}
 			}
+			if str[i] > 127 {
+				i += 2
+			}
+			i++
+			pos++
 		}
-		if str[i] > 127 {
-			i += 2
-		}
-		i++
-		pos++
 	}
 	return -1
 }
