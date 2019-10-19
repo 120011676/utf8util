d4fd7cc5406183a2a95d60772f570d30f6e59941
海东总司 <1252658389@qq.com>
2019-10-19 12:15:34 +0800

    v 2.0

--- README.md
+++ README.md
@@ -2,20 +2,26 @@
 * Golang gets the position of the Unicode character.
 * Golang 获取 Unicode 字符所在位置
 
-# Install
+# Install / 安装
 ```
 go get github.com/HeyDon-Lee/hdUnicode
 ```
 
-# Examples
-```
+# Examples / 代码实例
+```go
 package main
 
 import "github.com/HeyDon-Lee/hdUnicode"
 
 func main() {
+	// Get the position of the string
 	println(hdUnicode.UniIndex(`你好！世界`, `世界`))
 	println(hdUnicode.UniIndex(`こんにちは！世界`, `世界`))
 	println(hdUnicode.UniIndex(`안녕하세요！세계`, `세계`))
+
+	// Get the position of the character
+	println(hdUnicode.UniIndexOne(`你好！世界`, `世`))
+	println(hdUnicode.UniIndexOne(`こんにちは！世界`, `世`))
+	println(hdUnicode.UniIndexOne(`안녕하세요！세계`, `세`))
 }
 ```
--- indexOne.go
+++ indexOne.go
@@ -2,24 +2,21 @@ package hdUnicode
 
 import "strings"
 
-// Get the position of the string
-// UniIndex returns the index of the first instance of substr in s, or -1 if substr is not present in s.
-func UniIndex(str, substr string) int {
+// Get the position of the character
+// UniIndexOne returns the index of the first instance of substr in s, or -1 if substr is not present in s.
+func UniIndexOne(str, substr string) int {
 	switch strings.Index(str, substr) {
 	case 0:
 		return 0
 	default:
 		pos := 0
 		i := 0
-		substr_len := len(substr)
 		str_len := len(str)
 		for i < str_len {
 			if str[i] == substr[0] {
 				if str[i+1] == substr[1] {
 					if str[i+2] == substr[2] {
-						if str[i:i+substr_len] == substr {
-							return pos
-						}
+						return pos
 					}
 				}
 			}
